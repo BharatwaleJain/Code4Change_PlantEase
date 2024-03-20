763c61348cdc0cad613027c9c6a37280ee7b54ba
Aarjav Jain <aarjavkjain2@gmail.com>
2024-03-20 15:37:36 +0530

    update4pm

---
+++ js/active.js
@@ -0,0 +1,198 @@
+(function ($) {
+    'use strict';    
+
+    var browserWindow = $(window);
+
+    // Preloader Active Code
+    browserWindow.on('load', function () {
+        $('.preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
+
+    // Nav Active Code
+    if ($.fn.classyNav) {
+        $('#alazeaNav').classyNav();
+    }
+
+    // Search Active Code
+    $('#searchIcon').on('click', function () {
+        $('.search-form').toggleClass('active');
+    });
+    $('.closeIcon').on('click', function () {
+        $('.search-form').removeClass('active');
+    });
+
+    // Sliders Active Code
+    if ($.fn.owlCarousel) {
+        var welcomeSlide = $('.hero-post-slides');
+        var testiSlides = $('.testimonials-slides');
+        var portfolioSlides = $('.portfolio-slides');
+
+        welcomeSlide.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: false,
+            dots: false,
+            autoplay: true,
+            center: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1000
+        });
+
+        testiSlides.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 700,
+            animateIn: 'fadeIn',
+            animateOut: 'fadeOut'
+        });
+
+        portfolioSlides.owlCarousel({
+            items: 2,
+            margin: 30,
+            loop: true,
+            nav: true,
+            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+            dots: true,
+            autoplay: false,
+            autoplayTimeout: 5000,
+            smartSpeed: 700,
+            center: true
+        });
+    }
+
+    // Masonry Gallery Active Code
+    if ($.fn.imagesLoaded) {
+        $('.alazea-portfolio').imagesLoaded(function () {
+            // Filter items on button click
+            $('.portfolio-filter').on('click', 'button', function () {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+            // Initialize Isotope
+            var $grid = $('.alazea-portfolio').isotope({
+                itemSelector: '.single_portfolio_item',
+                percentPosition: true,
+                masonry: {
+                    columnWidth: '.single_portfolio_item'
+                }
+            });
+        });
+    }
+
+    // Magnific Popup Active Code
+    if ($.fn.magnificPopup) {
+        $('.portfolio-img, .product-img').magnificPopup({
+            gallery: {
+                enabled: true
+            },
+            type: 'image'
+        });
+        $('.video-icon').magnificPopup({
+            type: 'iframe'
+        });
+    }
+
+    // :: 7.0 Barfiller Active Code
+    if ($.fn.barfiller) {
+        $('#bar1').barfiller({
+            tooltip: true,
+            duration: 1000,
+            barColor: '#70c745',
+            animateOnResize: true
+        });
+        $('#bar2').barfiller({
+            tooltip: true,
+            duration: 1000,
+            barColor: '#70c745',
+            animateOnResize: true
+        });
+        $('#bar3').barfiller({
+            tooltip: true,
+            duration: 1000,
+            barColor: '#70c745',
+            animateOnResize: true
+        });
+        $('#bar4').barfiller({
+            tooltip: true,
+            duration: 1000,
+            barColor: '#70c745',
+            animateOnResize: true
+        });
+    }
+
+    // ScrollUp Active Code
+    if ($.fn.scrollUp) {
+        browserWindow.scrollUp({
+            scrollSpeed: 1500,
+            scrollText: '<i class="fa fa-angle-up"></i>'
+        });
+    }
+
+    // CounterUp Active Code
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+    }
+
+    // Sticky Active Code
+    if ($.fn.sticky) {
+        $(".alazea-main-menu").sticky({
+            topSpacing: 0
+        });
+    }
+
+    // Tooltip Active Code
+    if ($.fn.tooltip) {
+        $('[data-toggle="tooltip"]').tooltip();
+    }
+
+    // Price Range Active Code
+    $('.slider-range-price').each(function () {
+        var min = $(this).data('min');
+        var max = $(this).data('max');
+        var unit = $(this).data('unit');
+        var value_min = $(this).data('value-min');
+        var value_max = $(this).data('value-max');
+        var label_result = $(this).data('label-result');
+        var t = $(this);
+        $(this).slider({
+            range: true,
+            min: min,
+            max: max,
+            values: [value_min, value_max],
+            slide: function (event, ui) {
+                var result = label_result + " " + unit + ui.values[0] + ' - ' + unit + ui.values[1];
+                console.log(t);
+                t.closest('.slider-range').find('.range-price').html(result);
+            }
+        });
+    });
+
+    // Prevent default click action
+    $('a[href="#"]').on('click', function (event) {
+        event.preventDefault();
+    });
+
+    // Redirect to cart page
+    $('.single-product-area').on('click', function () {
+        window.location.href = 'cart.html';
+    });
+
+    // WOW Active Code
+    if (browserWindow.width() > 767) {
+        new WOW().init();
+    }
+
+})(jQuery);
